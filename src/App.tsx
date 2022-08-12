874021d40cf102b085be92418030610efea47229
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-12 20:20:20 +0200

    style: add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-black-600">
+        Give me one more shot
+      </h1>
+    </div>
+  );
 }
 
 export default App;
