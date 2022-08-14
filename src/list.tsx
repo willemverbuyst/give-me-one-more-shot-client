332be1d425f845816662ff89829b425c8b5e8b26
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-14 05:26:39 +0200

    feat: add list component

--- src/list.tsx
+++ src/list.tsx
@@ -1,7 +1,66 @@
-import { Typography } from '@mui/material'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import { Gender } from './models'
 
-function List(): JSX.Element {
-  return <Typography>list</Typography>
+function createData(
+  birthdate: string,
+  BSN: string,
+  email: string,
+  familyName: string,
+  gender: Gender,
+  givenName: string
+) {
+  return { birthdate, BSN, email, familyName, gender, givenName }
 }
 
-export default List
+const rows = [
+  createData(
+    '12/12/2000',
+    '999998948',
+    'ping@pong.io',
+    'Ping',
+    Gender.FEMALE,
+    'Pong'
+  ),
+]
+
+export default function List() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Family Name</TableCell>
+            <TableCell align="left">Given Name</TableCell>
+            <TableCell align="right">Birthdate</TableCell>
+            <TableCell align="left">Gender</TableCell>
+            <TableCell align="left">Email</TableCell>
+            <TableCell align="right">BSN</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.familyName}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell align="left" component="th" scope="row">
+                {row.familyName}
+              </TableCell>
+              <TableCell align="left">{row.givenName}</TableCell>
+              <TableCell align="right">{row.birthdate}</TableCell>
+              <TableCell align="left">{row.gender}</TableCell>
+              <TableCell align="left">{row.email}</TableCell>
+              <TableCell align="right">{row.BSN}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
