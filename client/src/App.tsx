aadf593dc15e306e76dcf911d32ab7ec252a2377
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-20 04:53:02 +0200

    chore: add react-query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,23 @@
 import { Grid, Typography } from '@mui/material'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './App.css'
+import { queryClient } from './reactQuery/queryClient'
 import BasicTabs from './tabs'
 
 function App() {
   return (
-    <Grid container spacing={2} columns={12}>
-      <Grid xs={12} item textAlign="center">
-        <Typography variant="h1">Give me one more shot</Typography>
+    <QueryClientProvider client={queryClient}>
+      <Grid container spacing={2} columns={12}>
+        <Grid xs={12} item textAlign="center">
+          <Typography variant="h1">Give me one more shot</Typography>
+        </Grid>
+        <Grid xs={12} item display="flex" justifyContent="center">
+          <BasicTabs />
+        </Grid>
       </Grid>
-      <Grid xs={12} item display="flex" justifyContent="center">
-        <BasicTabs />
-      </Grid>
-    </Grid>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
